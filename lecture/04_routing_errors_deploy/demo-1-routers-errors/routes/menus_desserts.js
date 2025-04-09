663e0e0b01e156fd738c7f9eaa619120d45ce473
Kyle <kmthayer@uw.edu>
2025-04-09 14:39:03 -0700

    add error handling demo

--- lecture/04_routing_errors_deploy/demo-1-routers-errors/routes/menus_desserts.js
+++ lecture/04_routing_errors_deploy/demo-1-routers-errors/routes/menus_desserts.js
@@ -9,4 +9,26 @@ router.get('/1', (req, res) => {
     res.send("Chocolate Cake")
 })
 
+// broken endpoint to show error
+router.get('/2', async (req, res) => {
+
+    // Fake an error to pretend  our database failed or something
+    throw(new Error("Loading desserts has failed! Here are the technical details:"))
+
+    res.send("dessert 2")
+})
+
+// error handling done (more) correctly
+router.get('/3', async (req, res) => {
+    try{
+        // Fake an error to pretend  our database failed or something
+        throw(new Error("Loading desserts has failed! Here are the technical details:"))
+
+        res.send("dessert 2")
+    } catch(err) {
+        console.log(err)
+        res.status(500).send("Error loading dessert")
+    }
+})
+
 export default router
