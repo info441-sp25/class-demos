adae61b54f7d77ea76ec8a51639dcecc78e0b4d3
Kyle <kmthayer@uw.edu>
2025-05-13 14:13:07 -0700

    finish reverse proxy demo

--- lecture/13_reverse_proxies/microservices-end/express-reverse-proxy/app.js
+++ lecture/13_reverse_proxies/microservices-end/express-reverse-proxy/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import { createProxyMiddleware } from 'http-proxy-middleware'
+import request from 'request'
 
 import usersRouter from './routes/users.js';
 
@@ -17,20 +19,34 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
 
-app.get('/api/double', (req, res) => {
-    let num = req.query.num
-    let doubled = num * 2
-    res.send("" + doubled)
+app.get('/api/double', createProxyMiddleware({
+    target: 'http://localhost:5000'
+}))
+
+const servers = ['http://localhost:6001', 'http://localhost:6002']
+let cur_server_index = 0
+app.get('/api/square', (req, res) => {
+    try{
+        cur_server_index = (cur_server_index + 1) % servers.length
+        req
+            .pipe(request({
+                url: servers[cur_server_index] + req.originalUrl
+            }))
+            .pipe(res)
+    }catch(err){
+        console.log("error in /api/square:", err)
+        res.status(500).json({status: "error", error: err})
+    }
 })
 
-app.get('/api/square', (req, res) => {
-    let num = req.query.num
-    let squared = num * num
-    res.send("" + squared)
-})
+app.use('/*', createProxyMiddleware({
+    target: 'http://localhost:4000',
+    pathRewrite: (path, req) => req.baseUrl,
+    changeOrigin: true
+}))
 
 export default app;
